4af2417eb46efd0e3974f7ee498cf734153887f6
Adi Stadelmann <spaced.wombat@gmail.com>
2016-03-15 21:09:45 +0100

    A Json example added

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -5,48 +5,44 @@ import org.singlespaced.d3js.d3
 
 import scala.scalajs.js
 
+@js.native
+trait MyRootJson extends js.Object {
+  val root:js.Array[MyJson]= js.native
+}
+
+
+
+@js.native
+trait MyJson extends js.Object {
+  val color:String= js.native
+}
+
 object ScalaJSExample extends js.JSApp {
 
   def main(): Unit = {
     /**
-      * Adapted from http://thecodingtutorials.blogspot.ch/2012/07/introduction-to-d3.html
+      * Answer for http://stackoverflow.com/questions/36010776/d3-json-parsing-in-scala-js
       */
-    val graphHeight = 450
 
-    //The width of each bar.
-    val barWidth = 80
 
-    //The distance between each bar.
-    val barSeparation = 10
-
-    //The maximum value of the data.
-    val maxData = 50
-
-    //The actual horizontal distance from drawing one bar rectangle to drawing the next.
-    val horizontalBarDistance = barWidth + barSeparation
-
-    //The value to multiply each bar's value by to get its height.
-    val barHeightMultiplier = graphHeight / maxData;
-
-    //Color for start
-    val c = d3.rgb("DarkSlateBlue")
-
-    val rectXFun = (d: Int, i: Int) => i * horizontalBarDistance
-    val rectYFun = (d: Int) => graphHeight - d * barHeightMultiplier
-    val rectHeightFun = (d: Int) => d * barHeightMultiplier
-    val rectColorFun = (d: Int, i: Int) => c.brighter(i * 0.5).toString
+    val rectXFunVpc = (d: MyJson, i: Int) => i * 30;
+    val rectColorFun =(d: MyJson) => d.color;
 
     val svg = d3.select("body").append("svg").attr("width", "100%").attr("height", "450px")
-    val sel = svg.selectAll("rect").data(js.Array(8, 22, 31, 36, 48, 17, 25))
-    sel.enter()
-      .append("rect")
-      .attr("x", rectXFun)
-      .attr("y", rectYFun)
-      .attr("width", barWidth)
-      .attr("height", rectHeightFun)
-      .style("fill", rectColorFun)
 
+    d3.json("json-example.json", (error: js.Any, json: js.Any) => {
 
+      val dataArray = json.asInstanceOf[MyRootJson].root
+
+      val sel = svg.selectAll("rect").data(dataArray)
+      sel.enter()
+        .append("rect")
+        .attr("x", rectXFunVpc)
+        .attr("y", 20)
+        .attr("width", 20)
+        .attr("height", 10)
+        .style("fill", rectColorFun)
+      ()
+    })
   }
-
 }
